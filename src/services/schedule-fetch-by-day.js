f1adaf22ae5ca54258b1d7551948bc26fd816b6d
João Paulo Dagostin <dagostinjota@gmail.com>
2025-05-22 10:33:57 +0200

    show result js

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,22 @@
+import { dayjs } from "dayjs";
+import { apiConfig } from "./api-config";
+
+export async function scheduleFetchByDay({ date }) {
+  try {
+    //Fazendo a requisicao:
+    const response = await fetch(`${apiConfig.baseURL}/schedules`);
+
+    //converte para JSON:
+    const data = await response.JSON();
+
+    //filtra os agendamentos pelo dia selecionado:
+    const dailySchedules = data.filter((schedule) =>
+      dayjs(date).isSame(schedule.when, "day")
+    );
+
+    return dailySchedules;
+  } catch (error) {
+    console.log(error);
+    alert("Nao foi possivel buscar os agendamentos do dia selecionado");
+  }
+}
